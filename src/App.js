31717cf1de4340a2be0ecc90560249ad433b0365
DaveBBL <dave.bbl@gmail.com>
2023-04-12 13:29:34 -0700

    Update Biography, Add Contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import NavSocial from "./features/NavSocial";
+import NavSocial from "./components/NavSocial";
 import Publicity from "./features/Publicity";
 import Videos from "./features/Videos";
 import Follow from "./features/Follow";
 import TourDates from "./features/TourDates";
 import Biography from "./features/Biography";
+import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Follow />
       <TourDates />
       <Biography />
+      <Contact />
     </div>
   );
 }
--- src/features/Biography.js
+++ src/features/Biography.js
@@ -1,26 +1,27 @@
 import Title from "../components/Title";
-import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from "reactstrap";
+import { Card, CardTitle, CardText, CardImg, CardImgOverlay, CardGroup } from "reactstrap";
 import outdoorphoto from "../app/assets/img/radu-florin-XmyHJfZ-qDc-unsplash.jpg";
 
 const Biography = () => {
   return (
     <div>
       <Title title="BIOGRAPHY" />
-      <Card>
-        <CardImg
-          src={outdoorphoto}
-          alt="Frank pensively leaning on a wall outside."
-          width="100%"
-        />
-        <CardImgOverlay>
+      <CardGroup>
+        <Card>
           <CardTitle tag="h4">Frank's Story</CardTitle>
           <CardText>
             Frank is a very interesting man. He plays Guitar and sings. He
             writes all his own songs.
           </CardText>
-        </CardImgOverlay>
-      </Card>
-      <p></p>
+        </Card>
+        <Card>
+          <CardImg
+            src={outdoorphoto}
+            alt="Frank pensively leaning on a wall outside."
+            width="100%"
+          />
+        </Card>
+      </CardGroup>
     </div>
   );
 };
