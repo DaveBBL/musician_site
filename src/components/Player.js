c11fa172cb602ddfb5428e8583273e815ef0e288
DaveBBL <dave.bbl@gmail.com>
2023-04-14 13:38:54 -0700

    Player will work, but needs work

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,11 +4,49 @@ import song2039 from "../app/assets/DKeH_2039.mp3"; // importing the music
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
 import { IconContext } from "react-icons"; // for customazing the icons
-import styles from './Player.module.css'
+import styles from "./Player.module.css";
 
 const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState({
+    min: "",
+    sec: "",
+  })
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sed: "",
+  });
+
+  const [seconds, setSeconds] = useState();
+
   const [play, { pause, duration, sound }] = useSound(song2039);
+  
+  useEffect(() => {
+    if (duration) {
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemain = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain,
+      });
+    };
+  }, [duration]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([])); // setting the seconds state with the current state
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec,
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
 
   const playingButton = () => {
     if (isPlaying) {
@@ -18,15 +56,40 @@ const Player = () => {
       play();
       setIsPlaying(true);
     }
-  }
+  };
 
   return (
     <div className={styles.component}>
       <h2>Playing Now</h2>
-      <img className={styles.musicCover} src="https://picsum.photos/200/200" alt="random" />
+      <img
+        className={styles.musicCover}
+        src="https://picsum.photos/200/200"
+        alt="random"
+      />
       <div>
         <h3 className={styles.title}>Frank Tuttle</h3>
         <p className={styles.subTitle}>30 Years Later</p>
+      </div>
+      <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
       </div>
       <div>
         <button className={styles.playButton}>
